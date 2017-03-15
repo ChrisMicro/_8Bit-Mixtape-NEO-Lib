0fc8e6b01a384d757b79c76da7033fe23cd801c4
Christoph Haberer <c.hab@gmx.net>
2017-03-15 10:47:42 +0100

    colorWheel instead of Wheel

--- examples/Blink_all_NEOs/Blink_all_NEOs.ino
+++ examples/Blink_all_NEOs/Blink_all_NEOs.ino
@@ -71,7 +71,7 @@ void setup()
 
 void loop() 
 {
-  setColorAllPixel(Wheel(220));       // all pixels set to pink using the Wheel() function to choose a color from the spectrum 0-255
+  setColorAllPixel(colorWheel(220));  // all pixels set to pink using the Wheel() function to choose a color from the spectrum 0-255
   pixels.show();                      // This sends the updated pixel color to the hardware.
   delay(1000);                        // wait for a while
 
